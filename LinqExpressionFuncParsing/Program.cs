882aa31db6174a63ac62384bf81ae833e7bfafe6
Yvain <yvain.tw@flexsystem.com>
2020-01-14 16:30:51 +0800

    循環拆解條件

--- LinqExpressionFuncParsing/Program.cs
+++ LinqExpressionFuncParsing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace LinqExpressionFuncParsing
@@ -7,11 +8,36 @@ namespace LinqExpressionFuncParsing
     {
         private static void Main(string[] args)
         {
-            //Expression<Func<int, bool>> funcExpression = num => num == 0 && num != 10 && num != 20;
+            Expression<Func<TestClass, bool>> funcExpression =
+                num => num.PropertyA == "A" && num.PropertyB != 0 && num.PropertyC;
+
+            var pExpression = funcExpression.Parameters[0]; //lambda 參數
+            var body = (BinaryExpression)funcExpression.Body; //lambda 主體
+
+            Console.WriteLine($"解析?：{body.Right.Type}");
+
+            if (body.Right is BinaryExpression right)
+            {
+                Console.WriteLine($"解析?：{pExpression.Name} => {right.Left} {right.NodeType} {right.Right}");
+            }
+
+            while (body.Left is BinaryExpression left)
+            {
+                Console.WriteLine($"解析：{pExpression.Name} => {body.Left} {body.NodeType} {body.Right}");
+                body = left;
+            }
+
+            Console.WriteLine($"解析 1：{pExpression.Name} => {body.Left} {body.NodeType} {body.Right}");
+            Console.WriteLine($"解析 2：{body}");
+            Console.ReadLine();
+        }
+
+        private static void BaseTypeParsing()
+        {
             Expression<Func<string, bool>> funcExpression = num => num == "A" && num != "B" && num != "C";
 
             var pExpression = funcExpression.Parameters[0]; //lambda 參數
-            var body = (BinaryExpression)funcExpression.Body;  //lambda 主體
+            var body = (BinaryExpression) funcExpression.Body; //lambda 主體
 
             Console.WriteLine($"解析：{pExpression.Name} => {body.Left} {body.NodeType} {body.Right}");
             Console.WriteLine($"解析：{body}");
---
+++ LinqExpressionFuncParsing/TestClass.cs
@@ -0,0 +1,20 @@
+﻿namespace LinqExpressionFuncParsing
+{
+    public class TestClass
+    {
+        /// <summary>
+        /// 屬性 A:string
+        /// </summary>
+        public string PropertyA { set; get; }
+
+        /// <summary>
+        /// 屬性 B:decimal
+        /// </summary>
+        public decimal PropertyB { set; get; }
+
+        /// <summary>
+        /// 屬性 C:bool
+        /// </summary>
+        public bool PropertyC { set; get; }
+    }
+}
